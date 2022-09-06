5169021ac9aadf4ea7420dba167f0beeaf848ea8
Luke Hackett <luke-hackett@outlook.com>
2022-09-05 19:54:23 -0700

    changed or to and statement

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
     ascii_val = ord(c)
-    if ascii_val >= 97 or ascii_val <= 123:
+    print(ascii_val)
+    if ascii_val >= 97 and ascii_val <= 123:
         return True
     else:
         return False
