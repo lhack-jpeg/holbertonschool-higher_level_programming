6577378f455c4ee30747fceadae8f317047098f5
Luke Hackett <luke-hackett@outlook.com>
2022-09-05 19:51:12 -0700

    change bool check on ord

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     ascii_val = ord(c)
-    if ascii_val > 96 or ascii_val < 123:
+    if ascii_val >= 97 or ascii_val <= 123:
         return True
     else:
         return False
