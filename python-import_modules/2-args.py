b27e57a213d3fc307c9a96b07b2da053bb9279e5
Luke Hackett <luke-hackett@outlook.com>
2022-09-06 19:29:03 -0700

    add name and main convention

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
-import sys
 
-argc = len(sys.argv) - 1
+if __name__ == '__main__':
+    import sys
 
-if (argc == 1):
-    print("{:d} argument:".format(argc))
-elif argc == 0:
-    print("{:d} arguments.".format(argc))
-else:
-    print("{:d} arguments:".format(argc))
+    argc = len(sys.argv) - 1
 
-for i in range(1, argc + 1):
-    print("{:d}: {:s}".format(i, sys.argv[i]))
+    if (argc == 1):
+        print("{:d} argument:".format(argc))
+    elif argc == 0:
+        print("{:d} arguments.".format(argc))
+    else:
+        print("{:d} arguments:".format(argc))
+
+        for i in range(1, argc + 1):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
