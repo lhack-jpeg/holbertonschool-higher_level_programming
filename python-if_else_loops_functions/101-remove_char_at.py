6f6fd49b1cbcdd9bd27497ba38b874de7b865119
Luke Hackett <luke-hackett@outlook.com>
2022-09-07 16:58:04 -0700

    add check for negative n

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -2,5 +2,8 @@
 
 
 def remove_char_at(str, n):
-    res_str = str[:n] + str[n+1:]
+    if n >= 0:
+        res_str = str[:n] + str[n+1:]
+    else:
+        res_str = str[:n-1] + str[n-1:]
     return res_str
