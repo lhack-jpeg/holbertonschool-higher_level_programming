1e047717dfddc1c62673fd283d6f7ff309748b0d
Luke Hackett <luke-hackett@outlook.com>
2022-09-12 21:36:19 -0700

    function return list of common elements

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+    common_items = []
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                common_items.append(i)
+
+    return common_items
