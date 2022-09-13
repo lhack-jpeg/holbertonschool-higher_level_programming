5d8a3c30619906267c44d9d4651b068287577ba8
Luke Hackett <luke-hackett@outlook.com>
2022-09-12 17:30:04 -0700

    add is not none check

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,10 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j < len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]), end=" ")
-            else:
-                print(matrix[i][j])
-    if len(matrix) == 0:
-        print()
+    if matrix is not None:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if j < len(matrix[i]) - 1:
+                    print("{:d}".format(matrix[i][j]), end=" ")
+                else:
+                    print("{:d}".format(matrix[i][j]))
