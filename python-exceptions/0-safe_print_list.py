a2d33cfbfc1dc7c1c571c4e3126f9963a97b9a3c
Luke Hackett <luke-hackett@outlook.com>
2022-09-13 20:47:17 -0700

    add error class

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             total_print += 1
         print()
-    except:
+    except IndexError:
         print()
 
     return total_print
