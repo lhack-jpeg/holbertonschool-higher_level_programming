7a7cbc6d89f4d6c4c7e4e241ac3a3eba3f164868
Luke Hackett <luke-hackett@outlook.com>
2022-09-13 20:43:34 -0700

    function to print elements from array

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    total_print = 0
+    try:
+        for i in range(0, x):
+            print(my_list[i], end="")
+            total_print += 1
+    except:
+        pass
+    print()
+    return total_print
