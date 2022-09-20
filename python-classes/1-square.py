2d1a918812ee12034d44c941133551be2188758e
Luke Hackett <luke-hackett@outlook.com>
2022-09-19 20:53:52 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Write a class that defines a square by the size attribute.'''
+
+
+class Square:
+    '''Creates an initalised object'''
+    def __init__(self, size=0):
+        self.__size = size
