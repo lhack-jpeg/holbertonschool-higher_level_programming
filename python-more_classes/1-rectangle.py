d65ffa6bf0d74cd6b8a94566b4050ff7c95de62c
Luke Hackett <luke-hackett@outlook.com>
2022-09-20 16:26:03 -0700

    Add private attributes Height and Width

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''Create class rectangle'''
+
+
+class Rectangle:
+    '''Creates a rectangle object'''
+
+    def __init__(self, width=0, height=0):
+        '''Initialisation of object. Runs check to ensure value passed
+        are good.'''
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        '''Return width of object.'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''Set width of object.'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        '''Return Height of object.'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''Set value of height of object.'''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
