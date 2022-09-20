f9e4b485e40de352b7e230204a2dadf3a0a332a3
Luke Hackett <luke-hackett@outlook.com>
2022-09-19 22:09:28 -0700

    Task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+'''Write a class that defines a square by the size attribute.'''
+
+
+class Square:
+    '''Creates an initalised object'''
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Returns the value of __size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter property to update the private field."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """Returns value of position tuple"""
+        return self.__position
+
+    @position.setter
+    def positon(self, value):
+        """Sets value of position attribute"""
+        if isinstance(value, tuple) and len(value) == 2:
+            if value[0] > 0 and value[1] > 0:
+                self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """Returns the value of the size squared."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints to the STDOUT the square, newline if size = 0."""
+        if self.__size == 0:
+            print()
+        else:
+            if self.position[1] > 0:
+                for i in range(0, self.position[1]):
+                    print()
+            for j in range(0, self.__size):
+                for space in range(self.position[0]):
+                    print(" ", end="")
+                for sign in range(self.__size):
+                    print("#", end="")
+                print()
