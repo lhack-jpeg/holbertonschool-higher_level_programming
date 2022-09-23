b7cae3e9fce8ce0590e874d78d77c4b3a9d93504
Luke Hackett <luke-hackett@outlook.com>
2022-09-22 22:13:26 -0700

    updated styling

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,6 +7,7 @@ import unittest
 
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     def test_max(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4, "Should be 4")
@@ -28,6 +29,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_list_none(self):
         with self.assertRaises(TypeError):
             max_integer(None)
+
 
 if __name__ == '__main__':
     unittest.main()
