f7140b5abde6bfb830b3f3b86ad204f3d4cc6162
Luke Hackett <luke-hackett@outlook.com>
2022-09-25 17:38:47 -0700

    add module documentation\

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''This module contains the constructor used to create a lockedclass
+object'''
+
+
 class LockedClass:
     '''A locked class that only lets instance attribute of first_name'''
     __slots__ = ['first_name']
