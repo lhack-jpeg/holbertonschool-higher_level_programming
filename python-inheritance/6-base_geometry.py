a74b0c4d71c1dcaf0562e85a9cdb7148d5072747
Luke Hackett <luke-hackett@outlook.com>
2022-09-27 15:47:56 -0700

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''This module contains the constructor for class
+BaseGeometry
+'''
+
+
+class BaseGeometry:
+    '''Creates object with public instance method'''
+    def area(self):
+        raise Exception("area() is not implemented")
