18c9125ad2cc43a8ff5f125e2aa15d2daf2bdb0b
Luke Hackett <luke-hackett@outlook.com>
2022-09-27 00:31:51 -0700

    add documentation

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-
+'''
+Module contains one function
+'''
 
 def is_kind_of_class(obj, a_class):
+    '''
+    wrapper function for isinstance
+    '''
     return isinstance(obj, a_class)
