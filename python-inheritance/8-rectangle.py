1e42ba293383c551efffa508726dd5ee8a87651b
Luke Hackett <luke-hackett@outlook.com>
2022-09-27 21:58:40 -0700

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+This module contains the constructor that inherits from
+the BaseGeometry Class.
+'''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Rectangle class constructor'''
+    def __init__(self, width, height):
+        try:
+            self.integer_validator('width', width)
+            self.__width = width
+        except Exception as e:
+            raise e
+
+        try:
+            self.integer_validator('height', height)
+            self.__height = height
+        except Exception as e:
+            raise e
