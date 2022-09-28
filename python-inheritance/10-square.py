f1d28519836e9e776b1118eb5a1d5d14c0a3e4fe
Luke Hackett <luke-hackett@outlook.com>
2022-09-27 22:12:44 -0700

    Task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+This module contains the constructor that inherits from
+the BaseGeometry Class.
+'''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    '''Square class constructor'''
+    def __init__(self, size):
+        try:
+            self.integer_validator('size', size)
+            self.__size = size
+        except Exception as e:
+            raise e
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def __str__(self):
+        return (f'[Square] {self.__size}/{self.__size}')
