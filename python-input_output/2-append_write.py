0f4ea6fae2fdba4eba8bf4ab654772c31d3752d3
Luke Hackett <luke-hackett@outlook.com>
2022-09-27 22:56:15 -0700

    fix typo

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -9,6 +9,6 @@ def append_write(filename="", text=""):
     '''
     Function to append to file
     '''
-    with open(filename, "a", enconding="utf-8") as f:
+    with open(filename, "a", encoding="utf-8") as f:
         char_written = f.write(text)
         return char_written
