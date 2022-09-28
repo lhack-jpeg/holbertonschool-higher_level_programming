8a26d6a0318a9eef6064fa506394278a87adece4
Luke Hackett <luke-hackett@outlook.com>
2022-09-27 22:55:09 -0700

    task 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+This module contains one function
+to append to file
+'''
+
+
+def append_write(filename="", text=""):
+    '''
+    Function to append to file
+    '''
+    with open(filename, "a", enconding="utf-8") as f:
+        char_written = f.write(text)
+        return char_written
