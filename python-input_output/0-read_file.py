fdeed5bf7a60e6caa28097c951c58ada0658b044
Luke Hackett <luke-hackett@outlook.com>
2022-09-27 22:44:32 -0700

    add documentaation for func

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,7 @@ This module contains the one function read_file
 
 
 def read_file(filename=""):
+    '''Func opens file and prints to STDOUT'''
     with open(filename, "r", encoding="utf-8") as f:
         for line in f:
             print(line, end="")
