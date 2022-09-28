5b6398eac0ebd50ecf8aaab3dcfe4e4b0e708a52
Luke Hackett <luke-hackett@outlook.com>
2022-09-27 19:44:55 -0700

    add task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+This module contains the function to check is class
+'''
+
+
+def is_same_class(obj, a_class):
+    '''Wrapper function'''
+    return (type(obj) == a_class)
