07f2f99875b59f63b2c4d5b6be97fbec0713d010
Luke Hackett <luke-hackett@outlook.com>
2022-09-28 18:04:33 -0700

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+This module contains the the class contstructor student.
+'''
+
+
+class Student():
+    '''Student class constructor.'''
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            stud_dict = dict()
+            keys = list(set(attrs).intersection(vars(self).keys()))
+            for key in keys:
+                stud_dict[key] = vars(self).get(key)
+            return stud_dict
