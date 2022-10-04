96d568e637eac3d562a8e6794c6190bf518818aa
Luke Hackett <luke-hackett@outlook.com>
2022-10-03 21:35:56 -0700

    add test for rectangle area

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -25,7 +25,7 @@ class TestRectangle(unittest.TestCase):
     def test_obj_id(self):
         my_obj = Rectangle(10, 2)
         my_obj_2 = Rectangle(4, 12, 0, 0, 24)
-        self.assertEqual(my_obj.id, 3)
+        self.assertEqual(my_obj.id, 4)
         self.assertEqual(my_obj_2.id, 24)
 
     def test_right_type(self):
@@ -41,6 +41,22 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             my_obj = Rectangle(2, 12, 1, None)
 
+        with self.assertRaises(TypeError):
+            my_obj = Rectangle(2, 4)
+            my_obj.height = "1"
+
+        with self.assertRaises(TypeError):
+            my_obj = Rectangle(2, 4)
+            my_obj.width = "School"
+
+        with self.assertRaises(TypeError):
+            my_obj = Rectangle(2, 4)
+            my_obj.y = [1]
+
+        with self.assertRaises(TypeError):
+            my_obj = Rectangle(2, 4)
+            my_obj.x = (1, 2)
+
 
     def test_good_value(self):
         with self.assertRaises(ValueError):
@@ -54,6 +70,29 @@ class TestRectangle(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             my_obj = Rectangle(2, 0, 0, -1, 24)
+
+        with self.assertRaises(ValueError):
+            my_obj = Rectangle(1, 2)
+            my_obj.width = -1
+
+        with self.assertRaises(ValueError):
+            my_obj.height = 0
+
+        with self.assertRaises(ValueError):
+            my_obj.x = -1
+
+        with self.assertRaises(ValueError):
+            my_obj.y = -1
+
+
+
+    def test_rectangle_area(self):
+        test_obj = Rectangle(4, 5)
+        self.assertEqual(test_obj.area(), 20)
+
+        test_obj.width = 6
+        self.assertEqual(test_obj.area(), 30)
+
 
 if __name__ == "__main__":
     unittest.main()
