09c27bd84e3444c1d50e3eceffe295f97eb45e15
Luke Hackett <luke-hackett@outlook.com>
2022-10-03 20:16:03 -0700

    add test for raising exception errors

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -13,21 +13,47 @@ class TestRectangle(unittest.TestCase):
         module_docs = "models.rectangle".__doc__
         self.assertTrue(len(module_docs) > 1)
 
-        class_docs = Recctangle.__doc__
+        class_docs = Rectangle.__doc__
         self.assertTrue(len(class_docs) > 1)
 
     def test_obj_class(self):
         my_obj = Rectangle(10, 2)
         self.assertTrue(isinstance(my_obj, Rectangle))
 
-        self.assertTrue(issubclass(my_obj, Base))
+        self.assertTrue(issubclass(type(my_obj), Base))
 
     def test_obj_id(self):
         my_obj = Rectangle(10, 2)
-        my_obj = Rectangle(4, 12, 0, 0, 24)
-        self.assertEqual(my_obj.id, 1)
+        my_obj_2 = Rectangle(4, 12, 0, 0, 24)
+        self.assertEqual(my_obj.id, 3)
         self.assertEqual(my_obj_2.id, 24)
 
+    def test_right_type(self):
+        with self.assertRaises(TypeError):
+            my_obj = Rectangle("Hello World", 2)
+
+        with self.assertRaises(TypeError):
+            my_obj = Rectangle(2, [2])
+
+        with self.assertRaises(TypeError):
+            my_obj = Rectangle(2, 12, 0, "1")
+
+        with self.assertRaises(TypeError):
+            my_obj = Rectangle(2, 12, 1, None)
+
+
+    def test_good_value(self):
+        with self.assertRaises(ValueError):
+            my_obj = Rectangle(2, -2)
+
+        with self.assertRaises(ValueError):
+            my_obj = Rectangle(2, 2, 0, -1, 24)
+
+        with self.assertRaises(ValueError):
+            my_obj = Rectangle(2, 12, -1, 1, 36)
+
+        with self.assertRaises(ValueError):
+            my_obj = Rectangle(2, 0, 0, -1, 24)
 
 if __name__ == "__main__":
     unittest.main()
