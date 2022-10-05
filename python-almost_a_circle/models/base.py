d9cb832095c04fe105247824ebb77c845e57a898
Luke Hackett <luke-hackett@outlook.com>
2022-10-04 23:23:20 -0700

    add to file method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -25,3 +25,16 @@ class Base():
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Write JSON string to file.'''
+        obj_list = []
+        if list_objs is not None:
+            for item in list_objs:
+                obj_list.append(item.to_dictionary())
+
+        filename = str(cls.__name__) + '.json'
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(obj_list))
