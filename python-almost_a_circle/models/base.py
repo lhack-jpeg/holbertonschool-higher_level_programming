628e2956311a4eb56590c68868ddae882b4671a0
Luke Hackett <luke-hackett@outlook.com>
2022-10-05 17:30:12 +1100

    change check for list dict

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         '''Returns a JSON string of list dictionaries.'''
-        if not list_dictionaries or list_dictionaries == '':
+        if list_dictionaries is None or list_dictionaries == '':
             return '[]'
         return json.dumps(list_dictionaries)
 
