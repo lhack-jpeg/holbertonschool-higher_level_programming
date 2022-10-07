7d5554afc4931d1b77b2d6d39554e870541f8d5d
lhack-jpeg <luke-hackett@outlook.com>
2022-10-07 11:59:27 +1100

    add docs

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
+        '''Initialisation of the instance.'''
         if id is not None:
             self.id = id
         else:
