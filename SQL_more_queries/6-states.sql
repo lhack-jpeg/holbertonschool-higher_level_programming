af45095053f19d9e6a04ee3b5829ab523a110cd5
Luke Hackett <luke-hackett@outlook.com>
2022-11-01 22:51:49 -0700

    Script to create states database

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- TASK 6: States Table
+-- Creates a database hbtn_0d_usa and table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+USE `hbtn_0d_usa`;
+
+CREATE TABLE IF NOT EXISTS `states`
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
