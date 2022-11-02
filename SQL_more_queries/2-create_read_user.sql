794c6ca1aad6f0dda6a7ed2ef5678897f9f4d77a
Luke Hackett <luke-hackett@outlook.com>
2022-11-01 23:24:32 -0700

    task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -6,6 +6,6 @@ CREATE USER IF NOT EXISTS
        'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- GRANT SELECT PRIVILEGES ONLY FOR USER
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 FLUSH PRIVILEGES;
