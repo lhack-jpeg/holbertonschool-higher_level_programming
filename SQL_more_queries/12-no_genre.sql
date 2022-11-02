8880acbef48bbf8e6b0654854502f190e4ac53fd
Luke Hackett <luke-hackett@outlook.com>
2022-11-02 15:53:49 -0700

    fix order by

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -4,5 +4,6 @@ SELECT
  	title,
 	tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres on id = tv_show_genres.show_id
-WHERE genre_id IS NULL;
+LEFT JOIN tv_show_genres ON id = tv_show_genres.show_id
+WHERE genre_id IS NULL
+ORDER BY title, genre_id;
