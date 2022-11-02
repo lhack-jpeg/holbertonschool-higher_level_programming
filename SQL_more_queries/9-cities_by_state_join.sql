f2e15771c4fb740ee8dae8b38f37338aa595c0ac
Luke Hackett <luke-hackett@outlook.com>
2022-11-01 23:07:07 -0700

    FIX typo

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,9 @@
 -- Task 9: Cities by States
 -- Return a list of ordered cities and included state name
 SELECT
-	cities.id as id,
+	cities.id AS id,
 	name,
-	states.name as name
+	states.name AS name
 from cities
 LEFT JOIN states on cities.state_id = states.id
 ORDER BY cities.id;
