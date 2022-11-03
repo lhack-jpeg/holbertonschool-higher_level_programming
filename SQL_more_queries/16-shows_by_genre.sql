59558370e27e4d74614409d6d9a5a73121e0111b
Luke Hackett <luke-hackett@outlook.com>
2022-11-02 17:21:17 -0700

    task 16

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- Task 11: Genre ID for all shows
+-- Script that list all shows in Database
+SELECT
+	title,
+	tv_genres.name
+from tv_shows
+LEFT OUTER JOIN tv_show_genres
+     ON tv_show_genres.show_id = id
+LEFT OUTER JOIN tv_genres
+      ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY title, genre_id;
