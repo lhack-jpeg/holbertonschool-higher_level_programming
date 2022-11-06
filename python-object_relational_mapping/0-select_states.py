71f96b826815762da710423ba973c81d7e056444
Luke Hackett <luke-hackett@outlook.com>
2022-11-07 10:23:53 +1100

    task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''This Module returns the states listed in the database'''
+
+
+if '__name__' == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    db_username = argv[1]
+    db_password = argv[2]
+    db_name = argv[3]
+
+    db = MySQLdb.connect(host='localhost', user=db_username, passwd=db_password, db=db_name)
+    cur = db.cursor()
+    sql_statement = "SELECT * FROM states ORDER BY id ASC"
+    results = cur.execute(sql_statement)
--- python-object_relational_mapping/README.md
+++ python-object_relational_mapping/README.md
@@ -1,2 +1,3 @@
 # Python - Object-relational mapping
+
 ## By Luke Hackett
