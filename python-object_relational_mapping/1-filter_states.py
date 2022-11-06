96e5fb119712303b77cf006faa019bce5b99636f
Luke Hackett <luke-hackett@outlook.com>
2022-11-06 15:47:13 -0800

    Task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''This Module returns the states listed in the database.'''
+
+
+if __name__ == '__main__':
+    '''Return all rows from the state tables ordered by ID'''
+
+    from sys import argv
+    import MySQLdb
+
+    db_username = argv[1]
+    db_password = argv[2]
+    db_name = argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=db_username,
+        passwd=db_password,
+        db=db_name)
+    cur = db.cursor()
+    sql_statement = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cur.execute(sql_statement)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
