7bf22994a86b9fe6ade76f006a9e6f898ba11c11
Luke Hackett <luke-hackett@outlook.com>
2022-11-07 10:25:24 +1100

    fixed formatting

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,11 @@ if '__name__' == '__main__':
     db_password = argv[2]
     db_name = argv[3]
 
-    db = MySQLdb.connect(host='localhost', user=db_username, passwd=db_password, db=db_name)
+    db = MySQLdb.connect(
+        host='localhost',
+        user=db_username,
+        passwd=db_password,
+        db=db_name)
     cur = db.cursor()
     sql_statement = "SELECT * FROM states ORDER BY id ASC"
     results = cur.execute(sql_statement)
