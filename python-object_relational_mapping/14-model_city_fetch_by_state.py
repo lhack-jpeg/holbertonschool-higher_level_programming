c77da2fbb3c87e9918ec8c72813406dc6833a385
Luke Hackett <luke-hackett@outlook.com>
2022-11-06 21:23:40 -0800

    Script to print all citites

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instances = session.query(City).join(State).all()
-    for obj in instances:
-        print(f'{obj.state_id}: ({obj.id}) {obj.name}')
+    instances = session.query(
+        City, State).filter(City.state_id == State.id).order_by(City.id)
+    for city, state in instances:
+        print(f'{state.name}: ({city.id}) {city.name}')
     session.close()
