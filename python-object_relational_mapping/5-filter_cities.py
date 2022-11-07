912099e2d3fd3a1f2a3b8859f3c1fd7afab43806
Luke Hackett <luke-hackett@outlook.com>
2022-11-06 17:24:44 -0800

    fix print format

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -25,11 +25,15 @@ if __name__ == '__main__':
         db=db_name)
 
     cur = db.cursor()
-    SQL = "SELECT cities.id, cities.name, states.name FROM cities"
+    SQL = "SELECT cities.name FROM cities"
     WHERE = f"WHERE states.name = '{user_input}'"
     JOIN = "INNER JOIN states ON cities.state_id = states.id"
     SQL_STATEMENT = f'{SQL} {JOIN} {WHERE}'
     cur.execute(SQL_STATEMENT)
     rows = cur.fetchall()
+    result_list = []
     for row in rows:
-        print(row)
+        for col in row:
+            result_list.append(col)
+
+    print(', '.join(result_list))
