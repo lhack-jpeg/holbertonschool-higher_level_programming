57c4ee45f9c6c6e76e6d2aa9d7aba2e770f5f6e4
Luke Hackett <luke-hackett@outlook.com>
2022-11-06 19:37:41 -0800

    Return list of states containing the letter a

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Script lists state that contain letter A using SQLalchemy.'''
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    '''Entry into script. If table is empty print nothing'''
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}",
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instances = session.query(State).filter(State.name.ilike('%a%'))
+    for obj in instances:
+        print(f'{obj.id}: {obj.name}')
