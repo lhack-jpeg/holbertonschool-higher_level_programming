53ff81e33687f2b19ac6662b9a334b93877ede43
Luke Hackett <luke-hackett@outlook.com>
2022-11-08 10:15:04 +1100

    Updated constrcutor for advanced tasks

---
+++ python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Module contains the class city constructor.'''
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    '''City Class constructor'''
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False, )
