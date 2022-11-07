c7cea44711e1e56603151441da6b5bb3ae358604
Luke Hackett <luke-hackett@outlook.com>
2022-11-07 15:58:13 +1100

    Constructor for city obj

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Module contains the class city constructor.'''
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import State
+
+Base = declarative_base()
+
+
+class City(Base):
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
