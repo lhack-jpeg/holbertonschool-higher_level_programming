b011cb876ee45a99efc8da5e51ab26bcc083c2f2
Luke Hackett <luke-hackett@outlook.com>
2022-11-06 18:55:36 -0800

    Return list of states using sql alchemy

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Script lists all states using SQLalchemy.'''
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    '''Entry into script.'''
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}",
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(f'{instance.id}: {instance.name}')
