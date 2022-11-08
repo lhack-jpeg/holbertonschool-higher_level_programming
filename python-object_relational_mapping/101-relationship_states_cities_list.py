81671521e46ca21302dde4fb35c889c71a2fefed
Luke Hackett <luke-hackett@outlook.com>
2022-11-07 18:04:16 -0800

    Use relationship instead of querying tables

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,26 +12,15 @@ if __name__ == "__main__":
     engine = create_engine(
         f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}",
         pool_pre_ping=True,
-    )
+        )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     q = session.query(
-        State, City).filter(
+        State).filter(
             State.id == City.state_id).order_by(
-                City.state_id, City.id)
-    city_list = []
+                State.id, City.id).all()
     for row in q:
-        city_list.append(row)
-
-    for i in range(len(city_list)):
-        try:
-            if city_list[i][0].id == city_list[i - 1][0].id:
-                pass
-            else:
-                state = city_list[i][0]
-                print(f'{state.id}: {state.name}')
-        except IndexError:
-            pass
-        city = city_list[i][1]
-        print(f"\t {city.id}: {city.name}")
+        print (f'{row.id}: {row.name}')
+        for city in row.cities:
+            print(f'\t{city.id}: {city.name}')
