e1c003c722e490f02f9bafb6f5e3fa5c4b2b20b1
Luke Hackett <luke-hackett@outlook.com>
2022-11-07 18:14:22 -0800

    Return printed list of cities using relationship link

---
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Script Lists out the state and the cities attached to them.'''
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}",
+        pool_pre_ping=True,
+        )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    q = session.query(
+        City).order_by(City.id).all()
+    for row in q:
+        print(f'{row.id}: {row.name} -> {row.state.name}')
