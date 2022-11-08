9018a32523d2252e17f7d15c505acc981e34e374
Luke Hackett <luke-hackett@outlook.com>
2022-11-07 17:42:04 -0800

    Removed debug print

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     engine = create_engine(
         f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}",
         pool_pre_ping=True,
-        echo=True)
+    )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
