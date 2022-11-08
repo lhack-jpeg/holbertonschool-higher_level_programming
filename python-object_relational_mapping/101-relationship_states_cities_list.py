fdba1d346723f6cb9d8a0a528040e4ad1ea5a5c0
Luke Hackett <luke-hackett@outlook.com>
2022-11-07 17:41:03 -0800

    Return print formatted list of States and cities

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -11,9 +11,27 @@ from relationship_city import City
 if __name__ == "__main__":
     engine = create_engine(
         f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}",
-        pool_pre_ping=True)
+        pool_pre_ping=True,
+        echo=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    q = session.query(State, City).filter(State.id == City.state_id)
-    dir(q)
+    q = session.query(
+        State, City).filter(
+            State.id == City.state_id).order_by(
+                City.state_id, City.id)
+    city_list = []
+    for row in q:
+        city_list.append(row)
+
+    for i in range(len(city_list)):
+        try:
+            if city_list[i][0].id == city_list[i - 1][0].id:
+                pass
+            else:
+                state = city_list[i][0]
+                print(f'{state.id}: {state.name}')
+        except IndexError:
+            pass
+        city = city_list[i][1]
+        print(f"\t {city.id}: {city.name}")
