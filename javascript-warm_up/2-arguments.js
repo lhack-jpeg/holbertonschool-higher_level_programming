ff3ff481731b0c5d0ba22b36e6ad23e77dc6a5bf
lhack-jpeg <luke-hackett@outlook.com>
2022-12-05 12:11:15 +1100

    fix up standards issue

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process');
-const argAmount = argv.length
+const argAmount = argv.length;
 
 if (argAmount === 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (argAmount === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
