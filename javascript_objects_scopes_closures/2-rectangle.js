dda56090a3eec424c383faaa939aecb24bb514c5
lhack-jpeg <luke-hackett@outlook.com>
2022-12-05 15:47:10 +1100

    return empty object if params not met

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(width, height) {
+  constructor (width, height) {
     if (width > 0 && height > 0) {
       this.width = width;
       this.height = height;
-    } else {
-      this.height;
-      this.width;
     }
   }
 }
