63518e7c3d35580cc71ad4d32e0eee37aff9e4a3
Luke Hackett <luke-hackett@outlook.com>
2022-12-04 17:09:31 -0800

    Script to print out arguments passed through

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process');
+const argAmount = argv.length
 
-const argAmount = argv.length;
-
-if (argAmount === 0) {
-  console.log('No argument');
-} else if (argAmount === 1) {
-  console.log('Argument found');
+if (argAmount === 2) {
+    console.log('No argument');
+} else if (argAmount === 3) {
+    console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
