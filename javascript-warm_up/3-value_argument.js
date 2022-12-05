ac7ad8e0ec1e802629ba1413c4e25dc2ec4cc0bb
lhack-jpeg <luke-hackett@outlook.com>
2022-12-05 12:24:35 +1100

    fix issue with undefined var

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -6,7 +6,7 @@ const argAmount = process.argv.length;
 if (argAmount === 2) {
   console.log('No argument');
 } else if (argAmount === 3) {
-  console.log(argv[2]);
+  console.log(process.argv[2]);
 } else {
   //  pass
 }
