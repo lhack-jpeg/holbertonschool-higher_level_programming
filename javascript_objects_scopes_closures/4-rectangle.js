c57b36713e278ad06360f74fee5107aefc9df92d
lhack-jpeg <luke-hackett@outlook.com>
2022-12-05 15:58:15 +1100

    add rotate and double methods

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+class Rectangle {
+  //  Only initialises if params are met
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  //  Doubles the width and height by 2
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+
+  //  Rotate swaps the height to the width and vice versa
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  //  Prints out the rectangle to the console
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rectString = '';
+      for (let y = 0; y < this.width; y++) {
+        rectString += 'X';
+      }
+      console.log(rectString);
+    }
+  }
+}
+
+module.exports = Rectangle;
