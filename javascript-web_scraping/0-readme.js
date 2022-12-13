0f4be76415932da0c1a138c9b1619840340320bd
Luke Hackett <luke-hackett@outlook.com>
2022-12-13 11:21:05 +1100

    fix semitstandard issues

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const process = require('process');
-const fs = require('fs')
+const fs = require('fs');
 
-const url = process.argv[2]
+const url = process.argv[2];
 
-fs.readFile(url, 'utf-8', function (err, data){
-    if (err){
-        console.log(err)
-    }
-    console.log(data);
-})
+fs.readFile(url, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+});
