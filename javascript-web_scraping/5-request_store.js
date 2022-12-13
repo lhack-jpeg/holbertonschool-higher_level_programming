b14c29f4bc59d51e61434112779731c13c8fd711
Luke Hackett <luke-hackett@outlook.com>
2022-12-13 12:57:52 +1100

    convert html and save to file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const process = require('process');
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.stringify(body);
+  let myString = data.toString();
+
+  myString = myString.replace(/\\n/g, '\n');
+  myString = myString.replace(/"/g, '');
+  console.log(myString);
+
+  fs.writeFile(filePath, myString, { encoding: 'utf-8' }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
