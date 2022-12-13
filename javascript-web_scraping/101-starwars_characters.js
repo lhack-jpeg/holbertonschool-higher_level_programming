635d04e40b546520c6648f6fb996999d56918b49
Luke Hackett <luke-hackett@outlook.com>
2022-12-13 16:11:42 +1100

    Print out characters in order from api

---
+++ javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const process = require('process');
+const request = require('request');
+
+const filmNumber = process.argv[2];
+
+const url = `https://swapi-api.hbtn.io/api/films/${filmNumber}`;
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  starWarsCharacters = {}
+  const characters = JSON.parse(body).characters;
+  printCharacter(characters, 0)
+});
+
+const printCharacter = (characters, index) => {
+  request.get(characters[index], (err, response, body) => {
+    if (err){
+      console.log(err);
+    }
+    console.log(JSON.parse(body).name)
+    if (index + 1 < characters.length){
+      printCharacter(characters, index + 1)
+    }
+  })
+}
