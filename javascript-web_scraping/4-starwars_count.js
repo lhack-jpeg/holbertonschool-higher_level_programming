d748f1d659294ee8744c24b34a8c941be39c8539
Luke Hackett <luke-hackett@outlook.com>
2022-12-13 12:13:06 +1100

    return count of wedge antilles

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const process = require('process');
+const request = require('request');
+
+
+
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body).results
+  let count = 0
+  data.forEach(element => {
+    const characterArray = element.characters
+    characterArray.forEach(element => {
+      if (element.includes('18')){
+        count++;
+      }
+    });
+  });
+  console.log(count);
+});
