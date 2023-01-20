ce23e6ec24318add7affd42b4057961016c51e40
Luke Hackett <luke-hackett@outlook.com>
2023-01-20 14:59:51 +1100

    move function to outside of main\

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 # This script fetches a url and prints out the body
+import urllib.request
 
-if __name__ == "__main__":
-    import urllib.request
+
+def url_fetcher():
+    """Function prints out information about the status of the server."""
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    url_fetcher()
