7f02553b1144c5955052f7dbdb0c9e61fa9016fe
Luke Hackett <luke-hackett@outlook.com>
2023-01-20 14:58:01 +1100

    return url request

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# This script fetches a url and prints out the body
+
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
