b3b9de268a793c081df4e153590e772ca6e4b247
Luke Hackett <luke-hackett@outlook.com>
2023-01-20 13:37:30 +1100

    fixed key value pair

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a post request to URL and displays body of the response
-curl -sX POST -F "email=test@gmail.com" -F "subject:I will always be here for PLD" "$1"
+curl -sX POST -F "email=test@gmail.com" -F "subject=I will always be here for PLD" "$1"
