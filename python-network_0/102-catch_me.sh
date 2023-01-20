adcca56d6d4034b3f51344a43cfea287a1e9ae40
Luke Hackett <luke-hackett@outlook.com>
2023-01-20 14:04:03 +1100

    add header to check

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script tries to get server to respond with catch me
-curl -LX PUT 0.0.0.0:5000/catch_me -F "user_id=98"
+curl -sLX PUT 0.0.0.0:5000/catch_me -F "user_id=98" -H "Origin: HolbertonSchool"
