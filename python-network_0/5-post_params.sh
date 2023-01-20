0997f7006d0e89e9651e94457cc4737f96738f30
Luke Hackett <luke-hackett@outlook.com>
2023-01-20 13:36:02 +1100

    change to F flag

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a post request to URL and displays body of the response
-curl -sX POST "$1" -H 'Content-Type: application/json' -d '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}'
+curl -sX POST "$1" -F "email=test@gmail.com" -F "subject:I will always be here for PLD"
