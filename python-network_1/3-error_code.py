75b28137b06070e26ada1961098e1920e568df7b
Luke Hackett <luke-hackett@outlook.com>
2023-01-24 10:40:42 +1100

    Script to handle any HTTPerror

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    This script sends a request to server and handles any HTTPerror.
+"""
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import sys
+
+
+def http_error():
+    """
+    Sends through a post request to url and handles any HTTPerrors
+    """
+    url = sys.argv[1]
+    try:
+        with urlopen(url) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except HTTPError as error:
+        print("Error code: {}".format(error))
+
+
+if __name__ == '__main__':
+    http_error()
