4408bb219b65fdf08763f512c93675f4e69cdac3
Luke Hackett <luke-hackett@outlook.com>
2023-01-24 09:56:16 +1100

    pushing to sandbox for testing

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    This script sends a post request to a server
+    with the email parameter sent through.
+"""
+from urllib.request import Request, urlopen
+import sys
+
+
+def post_email():
+    """
+    Sends through a post request to url.
+    """
+    url = sys.argv[1]
+    email = sys.argv[2]
+    headers = {"email": email}
+    post_request = Request(url, headers=headers)
+    with urlopen(post_request) as response:
+        print(response.status)
+        print(response.read())
+
+
+if __name__ == '__main__':
+    post_email()
