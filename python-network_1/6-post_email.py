1b86a47adf409a819993f21403079408bc53d38d
Luke Hackett <luke-hackett@outlook.com>
2023-01-24 12:56:48 +1100

    send post request to url

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Script uses the requests module to post to a url
+"""
+import requests
+from sys import argv
+
+
+def post_email():
+    """
+        Script uses the requests module to post to a url and send email value
+        through.
+    """
+    url = argv[1]
+    email = argv[2]
+    post_data = {"email": email}
+    response = requests.post(url, data=post_data)
+    response.encoding('utf-8')
+    print(response.text)
+
+
+if __name__ == "__main__":
+    post_email()
