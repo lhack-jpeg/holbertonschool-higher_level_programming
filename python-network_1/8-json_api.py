f3f4ad8e5540912da6ba432bb1dfdd6ab6b8311c
Luke Hackett <luke-hackett@outlook.com>
2023-01-24 13:24:48 +1100

    script to teest json api

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Script sends a post request to url with the letter as parameter
+"""
+import requests
+from sys import argv
+
+
+def search_api(q=""):
+    """
+    Letter will sent under the variable 'q'. If the response is properly
+    formatted JSON will return a display of [<id>] <name>.
+    """
+    if argv[1]:
+        q = argv[1]
+    post_data = {'q': q}
+    url = 'http://0.0.0.0:5000/search_user'
+    res = requests.post(url, data=post_data)
+    print(res.json())
+
+
+if __name__ == "__main__":
+    search_api()
